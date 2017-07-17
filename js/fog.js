178eabc53410d2e89252089f14d0d263c9727f1b
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-07-17 19:27:01 -0400

    The initial files to begin.

---
+++ js/fog.js
@@ -0,0 +1,32 @@
+/* 
+Stay in the Light Engine v0.1.1
+Last Updated: 2017-July-17
+Authors: 
+	William R.A.D. Funk - http://WilliamRobertFunk.com
+	Jorge Rodriguez - http://WilliamRobertFunk.com 
+*/
+
+// Wrapped fig object
+var FogWrapper = function() {
+	// Internally global game variables.
+	var Fog = {};
+
+	Fog.init = function() {
+		// Create Fog instance here
+		// Place center hole at center screen
+	};
+
+	Fog.move = function() {
+		// Move eye of the fog
+	};
+
+	Fog.expand = function() {
+		// Eye of fog gets one increment bigger
+	};
+
+	Fog.contract = function() {
+		// Eye of fog resets to smallest setting
+	};
+
+	return Fog;
+};
