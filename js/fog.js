a96b18362f9f29f51c6e7f30671f4c10cd21b943
Jorge Rodriguez <jito.rodriguez@outlook.com>
2017-07-30 14:57:40 -0400

    Adds initial fog masking new strategy.

--- js/fog.js
+++ js/fog.js
@@ -9,36 +9,29 @@ Authors:
 // Wrapped fog object
 var FogWrapper = function(container, center) {
 	var Fog = {};
-	var circle;
+	// Fog parameters and internal values
 	var radius;
+
+	//Fog internal objects / graphics
+	var circle;
+
+	//Fog images
+	var bgBACK = PIXI.Sprite.fromImage('./images/bgblue.jpg');
+
+	bgBACK.anchor.set(0.5);
+
+	bgBACK.x = center.x;
+	bgBACK.y = center.y;
+
+
+	var bgFRONT = PIXI.Sprite.fromImage('./images/colorbg.jpg');
+	bgFRONT.anchor.set(0.5);
+
+	//var mainSPRITE = PIXI.Sprite.fromImage;
+
 
 	Fog.init = function() {
 
-		radius = 50;
-		//Layering circles...
-		// var pixiCircle = new PIXI.Graphics();
-		// pixiCircle.lineStyle(0, 0xFF00FF);  //(thickness, color)
-		// pixiCircle.beginFill(0xFF00FF, 1);
-		// pixiCircle.drawCircle(center.x, center.y, 100);   //(x,y,radius)
-		// pixiCircle.endFill();
-		// container.addChild(pixiCircle);
-
-		// var pixiCircle2 = new PIXI.Graphics();
-		// pixiCircle2.lineStyle(0, 0x00ff00);  //(thickness, color)
-		// pixiCircle2.beginFill(0x00ff00, 0.4);
-		// pixiCircle2.drawCircle(center.x, center.y, 80);   //(x,y,radius)
-		// pixiCircle2.endFill();
-		// container.addChild(pixiCircle2);
-		//End of layering circles
-
-		//Masking Container with circle dimension. 
-		//*Removes ability from drawing in rest of container unless within mask.*
-		circle = new PIXI.Graphics();
-		circle.lineStyle(2, 0xFF00FF);  //(thickness, color)
-		circle.drawCircle(center.x, center.y, radius);   //(x,y,radius)
-		circle.endFill();
-		container.mask = circle;
-		container.addChild(circle);
 	};
 
 	Fog.move = function() {
