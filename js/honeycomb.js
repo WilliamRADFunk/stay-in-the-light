0d4faf7baea372253f25e365cb1c3391697ac815
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-07-30 19:32:18 -0400

    Upversions the files.

--- js/honeycomb.js
+++ js/honeycomb.js
@@ -1,6 +1,6 @@
 /* 
-Stay in the Light v0.0.1
-Last Updated: 2017-July-23
+Stay in the Light v0.0.2
+Last Updated: 2017-July-30
 Authors: 
 	William R.A.D. Funk - http://WilliamRobertFunk.com
 	Jorge Rodriguez - http://jitorodriguez.com/
