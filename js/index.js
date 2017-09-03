87a8ce04591f47e89eecdf4bdca16474bda759c9
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-09-03 16:46:20 -0400

    Updates last editted date on files updated.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /* 
 Stay in the Light v0.0.5
-Last Updated: 2017-August-13
+Last Updated: 2017-September-03
 Authors: 
 	William R.A.D. Funk - http://WilliamRobertFunk.com
 	Jorge Rodriguez - http://jitorodriguez.com/
