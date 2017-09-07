0b8547d99cd9ea62ec61da337db6ec554c8b57af
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-09-06 21:59:55 -0400

    Adds inactive loading bar object

---
+++ js/loading-bar.js
@@ -0,0 +1,51 @@
+/* 
+Stay in the Light v0.0.7
+Last Updated: 2017-September-09
+Authors: 
+	William R.A.D. Funk - http://WilliamRobertFunk.com
+	Jorge Rodriguez - http://jitorodriguez.com/
+*/
+
+// Wrapped loading bar object
+var LoadingBarWrapper = function(center) {
+	// Publicly accessible functionality.
+	var LoadingBar = {};
+	/**
+	 * variables accessible to everything internal to LoadingBarWrapper go here
+	 * aka starts with 'var'
+	**/
+
+	/**
+	 * internal constructors (like Tile in honeycomb.js) accessible to everything
+	 * internal to LoadingBarWrapper go here. aka starts with 'var' but requires 'new'.
+	 * Make sure constructors start with first letter capitolized.
+	**/
+
+	/**
+	 * functions accessible to everything internal to LoadingBarWrapper go here
+	 * aka starts with 'var'
+	**/
+
+	/**
+	 * variables accessible publicly from LoadingBarWrapper go here
+	 * aka starts with 'LoadingBar'
+	**/
+
+	/**
+	 * functions accessible publicly from LoadingBarWrapper go here
+	 * aka starts with 'LoadingBar'
+	**/
+
+	// Should decide where to instantiate the LoadingBar on the tile map,
+	// and setup any internal logic for the LoadingBar.
+	LoadingBar.init = function() {
+		
+	};
+
+	LoadingBar.takeTurn = function() {
+
+	};
+
+	// Return public api object at very end.
+	return LoadingBar;
+};
