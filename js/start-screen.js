2a1948889d197fbe4599e89c124eb544a57b4613
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-09-17 20:11:46 -0400

    Updates the versioning.

--- js/start-screen.js
+++ js/start-screen.js
@@ -1,6 +1,6 @@
 /* 
-Stay in the Light v0.0.11
-Last Updated: 2017-September-10
+Stay in the Light v0.0.12
+Last Updated: 2017-September-17
 Authors: 
 	William R.A.D. Funk - http://WilliamRobertFunk.com
 	Jorge Rodriguez - http://jitorodriguez.com/
