a639ad1d02997c5c5ebf7a02a6f523fb8a1b186b
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-10-31 19:08:48 -0400

    Commits the timer.js file. Whoops!

---
+++ js/timer.js
@@ -0,0 +1,106 @@
+/* 
+Stay in the Light v0.0.22
+Last Updated: 2017-October-28
+Authors: 
+	William R.A.D. Funk - http://WilliamRobertFunk.com
+	Jorge Rodriguez - http://jitorodriguez.com/
+*/
+
+// Wrapped timer object
+var TimerWrapper = function(center) {
+	// Publicly accessible functionality.
+	var Timer = {};
+	/**
+	 * variables accessible to everything internal to TimerWrapper go here
+	 * aka starts with 'var'
+	**/
+	var timerBox = new PIXI.Graphics();
+	var timerText = new PIXI.Graphics();
+
+	var time = 600;
+
+	/**
+	 * internal constructors (like Tile in honeycomb.js) accessible to everything
+	 * internal to TimerWrapper go here. aka starts with 'var' but requires 'new'.
+	 * Make sure constructors start with first letter capitolized.
+	**/
+
+	/**
+	 * functions accessible to everything internal to TimerWrapper go here
+	 * aka starts with 'var'
+	**/
+
+	// Determines which digits to show in timer, making sure extra preceding 0 if only one digit.
+	var calculateTime = function() {
+		var digits = '';
+		var minutes = Math.floor(time / 60);
+		if(minutes < 10) {
+			digits += '0';
+		}
+		digits += minutes.toString() + ':';
+		var seconds = time % 60;
+		if(seconds < 10) {
+			digits += '0';
+		}
+		digits += seconds.toString();
+		return digits;
+	};
+	// Calls all the draw components that sum up to make the whole timer.
+	drawTimer = function() {
+		drawTimerBox();
+		drawTimerDigits(calculateTime());
+	};
+	// Draw the border around the timer.
+	var drawTimerBox = function() {
+		timerBox.clear();
+		timerBox.lineStyle(10, 0xCFB53B, 0.5);
+		timerBox.drawRect(0, 0, 100, 50);
+		timerBox.endFill();
+	};
+	// Draw the actual numbers in the timer.
+	var drawTimerDigits = function(timeString) {
+		// Text being weird in PIXI, it can't just be cleared like a graphic.
+		Timer.container.removeChild(timerText);
+		timerText = new PIXI.Text(timeString, {fontFamily: 'Courier', fontSize: 24, fontWeight: 800, fill: 0xCFB53B, align: 'left'});
+		// Relative to the timer's renderer box.
+		timerText.x = 15;
+		timerText.y = 15;
+		Timer.container.addChild(timerText);
+	};
+
+	/**
+	 * variables accessible publicly from TimerWrapper go here
+	 * aka starts with 'Timer'
+	**/
+
+	// Simply returns time remaining in the round.
+	Timer.getTime = function() {
+		return time;
+	};
+	// Draws the initial timer box and starter time.
+	Timer.startTimer = function() {
+		drawTimer();
+	};
+	// Decreases time by one second and redraws the timer to reflect updated time.
+	Timer.tickTimer = function() {
+		time--;
+		// Never go into negative time.
+		if(time < 0) {
+			time = 0;
+		}
+		// Draw updated timer
+		drawTimer();
+	};
+
+	// Should decide where to instantiate the Timer on the tile map,
+	// and setup any internal logic for the Timer.
+	Timer.init = function() {
+		Timer.container = new PIXI.Container();
+
+		Timer.container.addChild(timerBox);
+		Timer.container.addChild(timerText);
+	};
+
+	// Return public api object at very end.
+	return Timer;
+};
