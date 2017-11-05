68d3e3091351bae085152f8e9feafd514cfbb436
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-11-04 23:30:12 -0400

    Updates the dates.

--- js/honeycomb.js
+++ js/honeycomb.js
@@ -1,6 +1,6 @@
 /* 
 Stay in the Light v0.0.23
-Last Updated: 2017-November-01
+Last Updated: 2017-November-04
 Authors: 
 	William R.A.D. Funk - http://WilliamRobertFunk.com
 	Jorge Rodriguez - http://jitorodriguez.com/
--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,6 @@
 /* 
 Stay in the Light v0.0.23
-Last Updated: 2017-October-28
+Last Updated: 2017-November-04
 Authors: 
 	William R.A.D. Funk - http://WilliamRobertFunk.com
 	Jorge Rodriguez - http://jitorodriguez.com/
